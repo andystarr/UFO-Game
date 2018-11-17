9f10074099f5077b5b11a8b8f5d0a8ba17b6de73
Andy Starr <andystarr@gmail.com>
2018-11-17 10:46:11 -0500

    Added Camera Controller
    Use player transform offset to add follow camera to 2d object.

---
+++ Assets/_Completed-Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        //transform by itself represents the gameobject it's attached to, in this case the camera.
+        offset = transform.position - player.transform.position;
+		
+	}
+
+    //LateUpdate is called once per frame but is guaranteed to run last
+    void LateUpdate ()
+    {
+        //move the camera relative to the player with offset
+        transform.position = player.transform.position + offset;
+		
+	}
+}
